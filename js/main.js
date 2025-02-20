66716e759be6f9a01c52acdc67d56859edf6b06c
NaamaP610 <naamap610@gmail.com>
2025-02-20 23:10:52 +0200

    style update

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@ const gGame = {
     secsPassed: 0,
     isFirstClick: true,
 }
-  
-function onInit (){
+
+function onInit() {
     gBoard = buildBoard()
     renderBoard(gBoard, '.board-container')
 
@@ -23,181 +23,128 @@ function onInit (){
     gGame.coveredCount = 0
     gGame.markedCount = 0
     gGame.isFirstClick = true
-    console.log('game!');
 
     var elSmileyBt = document.querySelector('.smiley-btn')
     elSmileyBt.innerText = '😃'
-    
+
 }
 
-function buildBoard(){
-    var size = gLevel.SIZE
-    const board = []
-
-    for (var i = 0; i < size; i++) {
-        board.push([])
-        for (var j = 0; j < size; j++) {
-            board[i][j] = {
-                minesAroundCount: 0,
-                isCovered: true,
-                isMine: false,
-                isMarked: false,              
-            }  
-        }
-    }
-
-    // board[3][2].isMine = true
-    // board[2][3].isMine = true
-
-    // setRandMine(gLevel.MINES , board)
-
-    console.log(board);
-    return board
-}
-
-function renderBoard(mat, selector) {
-
-    var strHTML = '<table><tbody>'
-    for (var i = 0; i < mat.length; i++) {
-
-        strHTML += '<tr>'
-        for (var j = 0; j < mat[0].length; j++) {
-
-            const cell = mat[i][j]
-            const className = `cell cell-${i}-${j} ${cell.isCovered ? 'covered' : 'revealed'}`
-
-            strHTML += `<td class="${className}" onclick="onCellClicked(event, ${i}, ${j})" oncontextmenu="onCellClicked(event, ${i}, ${j})">
-            ${cell.isMine ? '💣' : cell.minesAroundCount} 
-            </td>`
-
-        }
-        strHTML += '</tr>'
-    }
-    strHTML += '</tbody></table>'
-    
-    const elContainer = document.querySelector(selector)
-    elContainer.innerHTML = strHTML
-    elContainer.addEventListener("contextmenu", (e) => {
-            e.preventDefault();
-    })
-}
-
-function setMinesNegsCount(cellI ,cellJ, board){
-    var negsCount = 0 
+function setMinesNegsCount(cellI, cellJ, board) {
+    var negsCount = 0
 
     for (var i = cellI - 1; i <= cellI + 1; i++) {
-		if (i < 0 || i >= board.length) continue
-		for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-			if (j < 0 || j >= board[i].length) continue
-			if (i === cellI && j === cellJ) continue
-			if (board[i][j].isMine) negsCount++
-		}
-	}
-	return negsCount
+        if (i < 0 || i >= board.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= board[i].length) continue
+            if (i === cellI && j === cellJ) continue
+            if (board[i][j].isMine) negsCount++
+        }
+    }
+    return negsCount
 }
 
-function onCellClicked(event, i, j){
+function onCellClicked(event, i, j) {
     if (!gGame.isOn) return
-    
-    if (gGame.isFirstClick){
-        handelFirstClick(i,j)
+
+    if (gGame.isFirstClick) {
+        handelFirstClick(i, j)
     }
-      
+
     var elCell = document.querySelector(`.cell-${i}-${j}`)
-    console.log(i, j);
     var cerCell = gBoard[i][j]
-    
-    if (event.button === 0 && !gBoard[i][j].isMarked){
+
+    if (event.button === 0 && !gBoard[i][j].isMarked) {
         onCellRevel(elCell, i, j)
-        expandUncover(gBoard, i, j )
-        if (cerCell.isMine){
+        expandUncover(gBoard, i, j)
+        if (cerCell.isMine) {
             exposeMines(gBoard)
             checkGameOver(false)
-        } 
+        }
 
-    } else if (event.button === 2){
+    } else if (event.button === 2) {
         onCellMarked(elCell, i, j)
     }
-    
-    var totalSafeCells = (gLevel.SIZE**2) - gLevel.MINES 
-    if (gGame.coveredCount === totalSafeCells && countMarkedMinds(gBoard)){
+
+    var totalSafeCells = (gLevel.SIZE ** 2) - gLevel.MINES
+    if (gGame.coveredCount === totalSafeCells && countMarkedMinds(gBoard)) {
         checkGameOver(true)
     }
 }
 
 function handelFirstClick(i, j) {
     gGame.isFirstClick = false
-    setRandMine(gLevel.MINES , gBoard, {i , j}) 
-    
+    setRandMine(gLevel.MINES, gBoard, { i, j })
+
     for (var i = 0; i < gLevel.SIZE; i++) {
         for (var j = 0; j < gLevel.SIZE; j++) {
             gBoard[i][j].minesAroundCount = setMinesNegsCount(i, j, gBoard);
         }
     }
-    
+
     renderBoard(gBoard, '.board-container')
 }
 
 
-function onCellMarked(elCell , i ,j ){
+function onCellMarked(elCell, i, j) {
     if (!gBoard[i][j].isCovered) return
-    
-    if (!gBoard[i][j].isMarked){
+
+    if (!gBoard[i][j].isMarked) {
         gBoard[i][j].isMarked = true
         gGame.markedCount++
-        
+
         elCell.classList.remove('covered')
         elCell.classList.add('flagged')
         elCell.innerText = '🚩'
 
-    } else if (gBoard[i][j].isMarked){
+    } else if (gBoard[i][j].isMarked) {
         gBoard[i][j].isMarked = false
         gGame.markedCount--
-        
+
         elCell.classList.remove('flagged')
         elCell.classList.add('covered')
         elCell.innerText = gBoard[i][j].minesAroundCount
     }
-} 
+}
 
-function onCellRevel(elCell , i ,j ){
+function onCellRevel(elCell, i, j) {
     if (gBoard[i][j].isMarked) return
     if (!gBoard[i][j].isCovered) return
     gBoard[i][j].isCovered = false
     gGame.coveredCount++
-    console.log(gGame.coveredCount);
 
     elCell.classList.remove('covered')
     elCell.classList.add('revealed')
 }
 
-function expandUncover(board, i, j){
-    if (board[i][j].minesAroundCount === 0){
+function expandUncover(board, i, j) {
+    if (board[i][j].isMine) return
+    if (board[i][j].minesAroundCount === 0) {
         for (var x = i - 1; x <= i + 1; x++) {
             if (x < 0 || x >= board.length) continue
             for (var y = j - 1; y <= j + 1; y++) {
                 if (y < 0 || y >= board[i].length) continue
                 var NeighborCell = document.querySelector(`.cell-${x}-${y}`)
-                if(!board[x][y].isCovered) continue
-                onCellRevel(NeighborCell , x, y )
+                if (!board[x][y].isCovered) continue
+                onCellRevel(NeighborCell, x, y)
             }
         }
     }
 }
 
-function exposeMines(board){
+function exposeMines(board) {
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board.length; j++) {
             if (board[i][j].isMine) {
                 var elCell = document.querySelector(`.cell-${i}-${j}`)
                 onCellRevel(elCell, i, j)
                 elCell.classList.add('mine')
+                elCell.innerText = '💣'
             }
         }
     }
 }
 
-function countMarkedMinds(board){
+function countMarkedMinds(board) {
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board.length; j++) {
             if (board[i][j].isMine && !board[i][j].isMarked) return false
@@ -215,7 +162,7 @@ function setRandMine(level, board, firstClickPos) {
         var randJ = getRandomInt(0, board[0].length)
 
         if (randI === firstClickPos.i && randJ === firstClickPos.j) continue
-        
+
         if (!board[randI][randJ].isMine) {
             board[randI][randJ].isMine = true
             placedMines++
--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,56 @@
 'use strict'
 
-function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+function buildBoard() {
+  var size = gLevel.SIZE
+  const board = []
+
+  for (var i = 0; i < size; i++) {
+    board.push([])
+    for (var j = 0; j < size; j++) {
+      board[i][j] = {
+        minesAroundCount: 0,
+        isCovered: true,
+        isMine: false,
+        isMarked: false,
+      }
+    }
   }
-  
+
+  // console.log(board);
+  return board
+}
+
+function renderBoard(mat, selector) {
+
+  var strHTML = '<table><tbody>'
+  for (var i = 0; i < mat.length; i++) {
+
+    strHTML += '<tr>'
+    for (var j = 0; j < mat[0].length; j++) {
+
+      const cell = mat[i][j]
+      const className = `cell cell-${i}-${j} ${cell.isCovered ? 'covered' : 'revealed'}`
+
+      strHTML += `<td class="${className}" onclick="onCellClicked(event, ${i}, ${j})" oncontextmenu="onCellClicked(event, ${i}, ${j})">
+          ${cell.isMine ? '💣' : cell.minesAroundCount === 0 ? ' ' : cell.minesAroundCount} 
+          </td>`
+
+    }
+    strHTML += '</tr>'
+  }
+  strHTML += '</tbody></table>'
+
+  const elContainer = document.querySelector(selector)
+  elContainer.innerHTML = strHTML
+  elContainer.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  })
+}
+
+
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
